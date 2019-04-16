d3f00fbb0831cc90a587e4c2804df503f4bb668f
Adir Israeli <Adir.Israeli@docusign.com>
2019-04-16 16:35:36 +0300

    commit

---
+++ moduls.py
@@ -0,0 +1,56 @@
+#
+# # #
+# # # #function creation
+# # def printMyName():
+# #     print("Daniel")
+# # #
+# # # #entry point +call for function
+# # if __name__ == '__main__':
+# #     printMyName()
+# #     printMyName()
+# #     printMyName()
+# #     printMyName()
+#
+#
+# #second example
+# def run():
+#     return "The world is yours"
+#
+#
+# if __name__ == '__main__':
+#     kkk = run()
+# #
+# def run():
+#     return "hello"
+#
+# if __name__ == '__main__':
+#     my_word = run()
+
+
+#
+# # funtion can call several values in parralel
+# def printMyName(x, y, z):
+#    print(x,y,z)
+#
+# if __name__ == '__main__':
+#     printMyName(6, 4, 7)
+#
+
+
+
+
+# adding global variable
+#
+# o = 0
+
+# def printMyName(x, y, z):
+#    print(x,y,z)
+#
+def run(y):
+
+    return "hello "+ y
+
+if __name__ == '__main__':
+    shimi = run("Daniel")
+
+
